54ee4bb73b306acbde50ac263f853fbc0a8ac732
AnumNaz424 <159616271+AnumNaz424@users.noreply.github.com>
2024-02-28 12:16:00 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+const answer:{
+    "No1": number,
+    "No2": number,
+    "operator":string,
+}
+= await inquirer.prompt([{
+type:"number",
+name:"No1",
+message:"Please enter your No.1:",
+},
+{
+    type:"number",
+    name:"No2",
+    message:"Please enter your No.2:",
+},
+{
+        type:"list",
+        name:"operator",
+        message:"select your operator:",
+        choices: ["+", "-", "*", "/"]
+}
+])
+const {No1,No2,operator} = answer
+if(No1 && No2 && operator) {
+    let result:number = 0
+if(operator ==="+"){
+result = No1 + No2
+} else if(operator ==="-"){
+    result = No1 - No2
+}else if(operator ==="*"){ 
+    result = No1 * No2
+}else if(operator === "/"){
+    result = No1 / No2
+} console.log("your answer is " + result)
+}else{
+console.log("please select a valid operator")
+}
